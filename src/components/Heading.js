9ca61872195ca863bbfe7b8540a6f6c06efcb5d7
rayan-15 <rayan.r.shaikh@gmail.com>
2022-08-12 16:45:48 +0530

    first commit

---
+++ src/components/Heading.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = styled.header`
+  max-width: 70rem;
+  margin: 2rem auto;
+  text-align: center;
+`;
+
+const H1 = styled.h1`
+  font-family: 'Oswald', sans-serif;
+  margin-bottom: 1em;
+`;
+
+
+
+export const Heading = () => {
+
+  return (
+    <Header>
+      <H1>Unsplash</H1>
+      <p>The internet’s source of freely usable images.</p>
+      <p>Powered by creators everywhere.</p>
+      {}
+    </Header>
+  )
+}
